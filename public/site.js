6808d53bd296344d0abac80d449f676b03d8b02e
Alan Berndt <alan@eatabrick.org>
2014-04-12 12:07:52 -0700

    Improve volume control

--- public/site.js
+++ public/site.js
@@ -30,11 +30,24 @@ $(function() {
     });
   });
 
-  $('#volume').click(function(e) {
-    var volume = e.offsetX / $(this).width();
-
+  var adjusting = false;
+  var set_volume = function(volume) {
     $('#radio')[0].volume = volume;
     $('#volume p').css('width', volume * 100 + '%');
+  };
+
+  $('#volume').mousedown(function(e) {
+    e.preventDefault();
+    adjusting = true;
+    set_volume(e.offsetX / $(this).width());
+  }).mouseup(function(e) {
+    e.preventDefault();
+    adjusting = false;
+  }).mouseleave(function(e) {
+    e.preventDefault();
+    adjusting = false;
+  }).mousemove(function(e) {
+    if (adjusting) set_volume(e.offsetX / $(this).width());
   });
 
   var song_id;
